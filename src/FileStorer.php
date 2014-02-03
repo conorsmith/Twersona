a88327189b3ec1f06e68e10ca3eb9f4acd0099ae
Conor Smith <conorjbsmith@gmail.com>
2014-02-03 15:53:47 +0000

    Refactored FileStorer to use Flysystem
    I setup the FileStorer class to replace its calls to native PHP file functions with the Flysystem package using its local storage adapter.

--- src/FileStorer.php
+++ src/FileStorer.php
@@ -2,14 +2,20 @@
 
 namespace Twersona;
 
+use League\Flysystem\Filesystem;
+use League\Flysystem\Adapter\Local as Adapter;
+
 class FileStorer implements StorerInterface
 {
+    protected $filesystem;
     protected $file;
 
     public function __construct($file = null)
     {
+        $this->filesystem = new Filesystem(new Adapter(__DIR__.'/..'));
+
         if (is_null($file)) {
-            $this->file = dirname(__FILE__) . '/../storage/profile-cache';
+            $this->file = 'storage/profile-cache';
         } else {
             $this->file = $file;
         }
@@ -17,16 +23,16 @@ class FileStorer implements StorerInterface
 
     public function hasCachedData()
     {
-        return file_exists($this->file) && filesize($this->file) > 0;
+        return $this->filesystem->has($this->file) && $this->filesystem->getSize($this->file) > 0;
     }
 
     public function fetch()
     {
-        return json_decode(file_get_contents($this->file));
+        return json_decode($this->filesystem->read($this->file));
     }
 
     public function store($data)
     {
-        file_put_contents($this->file, json_encode($data));
+        $this->filesystem->put($this->file, json_encode($data));
     }
 }
